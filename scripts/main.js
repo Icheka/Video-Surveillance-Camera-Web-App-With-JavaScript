b9e039d67d597ac371caa8e08bed32d5ac7e1522
Icheka <rhemafortune@gmail.com>
2021-01-22 13:56:28 +0100

    Finished the framework for the web app. About to test now.

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+// Input stream config
+const conf = { video: { facingMode: "user" }, audio: false };
+
+const videoWindow = document.getElementById("video-window");
+const cameraImg = document.getElementById("camera_img");
+const cameraRenderer = document.getElementById("camera_renderer");
+const shutterControl = document.getElementById("shutter-control");
+
+
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(conf)
+        .then(stream => {
+	        track = stream.getTracks()[0];
+	        videoWindow.srcObject = stream;
+	    }).catch( err => {
+	        console.error("Error getting video stream :>> ", error);
+    });
+}
+
+shutterControl.onclick = function() {
+    cameraRenderer.width = cameraView.videoWidth;
+    cameraRenderer.height = cameraView.videoHeight;
+    cameraRenderer.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraImg.src = cameraSensor.toDataURL("image/webp");
+    cameraImg.classList.add("taken");
+};
+
+window.addEventListener("load", cameraStart, false);
